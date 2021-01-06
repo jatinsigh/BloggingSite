0ea38dd736913cdbbd955d8d8d4f1f39ac59c588
jatin singh <jatinsigh1810@gmail.com>
2021-01-06 12:00:19 +0530

    Updated Settings

--- todo_manager/settings.py
+++ todo_manager/settings.py
@@ -21,12 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's@83d#3uuafsh=_xls!#+s69y5ekc*#(1pqsg+3-w!h^0m_s6j'
+#SECRET_KEY = 's@83d#3uuafsh=_xls!#+s69y5ekc*#(1pqsg+3-w!h^0m_s6j'
+
+SECRET_KEY=os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['techieblogapp.herokuapp.com']
 
 
 # Application definition
