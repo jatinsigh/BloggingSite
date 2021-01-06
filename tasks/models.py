bcb7224964b328a6b3ec72d0396a11dfbf289e68
jatin singh <jatinsigh1810@gmail.com>
2021-01-06 11:07:25 +0530

    Another Commit

--- tasks/models.py
+++ tasks/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 class Activities(models.Model) :
-    title = models.CharField(max_length=100,default='Hero')
+    title = models.CharField(max_length=100,default='Title')
     content=models.TextField()
     date_posted=models.DateTimeField(auto_now=True)
     author=models.ForeignKey(User,on_delete=models.CASCADE)
