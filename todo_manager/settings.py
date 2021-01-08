1e8d019a60cd75bc1c394a84eac0092dab9cf996
jatin singh <jatinsigh1810@gmail.com>
2021-01-08 13:21:45 +0530

    Updated Settings

--- todo_manager/settings.py
+++ todo_manager/settings.py
@@ -29,9 +29,9 @@ SECRET_KEY=os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG = True #(os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mytechieblogapp.herokuapp.com']
 
 
 # Application definition
