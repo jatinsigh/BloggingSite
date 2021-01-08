c23f81d817b95030fa90612416f7b2b546590804
jatin singh <jatinsigh1810@gmail.com>
2021-01-08 11:38:49 +0530

    Image Settings Updated

--- todo_manager/settings.py
+++ todo_manager/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 import django_heroku
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -144,3 +146,16 @@ EMAIL_HOST_USER=os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_PASS')
 
 django_heroku.settings(locals())
+
+AWS_ACCESS_KEY_ID=os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY=os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME=os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
+
+
+AWS_S3_FILE_OVERWRITE=False
+AWS_DEFAULT_ACL=None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+S3_USE_SIGV4 = os.environ.get('S3_USE_SIGV4')
--- users/models.py
+++ users/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from PIL import Image
 
 # Create your models here.
 class Profile(models.Model):
@@ -10,12 +9,12 @@ class Profile(models.Model):
     def __str__(self):
         return (self.user)
 
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
+#    def save(self,*args,**kwargs):
+#        super().save(*args,**kwargs)
 
-        img=Image.open(self.image.path)
+#        img=Image.open(self.image.path)
 
-        if img.height>300 or img.width>300 :
-            output_size=(300,300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+#        if img.height>300 or img.width>300 :
+#            output_size=(300,300)
+#            img.thumbnail(output_size)
+#            img.save(self.image.path)
