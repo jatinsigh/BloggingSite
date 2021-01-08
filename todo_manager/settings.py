7308fd135add0b3ac5c3e75e778ab8a9e9786a7e
jatin singh <jatinsigh1810@gmail.com>
2021-01-08 13:25:46 +0530

    change Settings

--- todo_manager/settings.py
+++ todo_manager/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY=os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True #(os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['mytechieblogapp.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -148,14 +148,14 @@ EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_PASS')
 
 #django_heroku.settings(locals())
 
-AWS_ACCESS_KEY_ID=os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY=os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME=os.environ.get('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
+#AWS_ACCESS_KEY_ID=os.environ.get('AWS_ACCESS_KEY_ID')
+#AWS_SECRET_ACCESS_KEY=os.environ.get('AWS_SECRET_ACCESS_KEY')
+#AWS_STORAGE_BUCKET_NAME=os.environ.get('AWS_STORAGE_BUCKET_NAME')
+#AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
 
 
-AWS_S3_FILE_OVERWRITE=False
-AWS_DEFAULT_ACL=None
+#AWS_S3_FILE_OVERWRITE=False
+#AWS_DEFAULT_ACL=None
 
 #DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
