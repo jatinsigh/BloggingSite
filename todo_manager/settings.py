27f41e09fca9a59b62fa6e4e6524b29d467087cc
jatin singh <jatinsigh1810@gmail.com>
2021-01-08 11:46:12 +0530

    Updated Settings

--- todo_manager/settings.py
+++ todo_manager/settings.py
@@ -128,10 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-MEDIA_URL='/media/'
+MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK='bootstrap4'
 
